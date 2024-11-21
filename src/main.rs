8b1c109b71094543f7cf56811daaa6ce75d993a8
Chirag Kumar <kzs9@cdc.gov>
2024-11-21 16:51:55 -0500

    Add parameter loading from command-line specified input file (#3)
    * cargo pre-commit skeleton
    
    * make the repo be the whole crate
    
    * read in command line args and input params
    
    * add documentation for command line arguments
    
    * code commenting
    
    * remove -p option to not specify report names in command line args

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,48 @@
-use ixa::{context::Context, error::IxaError};
+use clap::Parser;
+use ixa::{
+    context::Context, error::IxaError, global_properties::ContextGlobalPropertiesExt,
+    random::ContextRandomExt,
+};
+use parameters_loader::Parameters;
+use std::path::PathBuf;
 
-// note this clippy complain will be removed once
-// initialize actually does something that may error
-#[allow(clippy::unnecessary_wraps)]
-fn initialize() -> Result<Context, IxaError> {
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// path to the input file
+    #[arg(short, long)]
+    input_file: PathBuf,
+
+    /// path to the output directory
+    #[arg(short, long)]
+    output_directory: PathBuf,
+}
+
+mod parameters_loader;
+
+fn initialize(args: &Args) -> Result<Context, IxaError> {
     let mut context = Context::new();
-    context.add_plan(0.0, |_context| {});
+    // read the global properties, setting them as parameters
+    // propagate any errors that may arise with the ? operator
+    parameters_loader::init_parameters(&mut context, &args.input_file)?;
+    let parameters = context
+        .get_global_property_value(Parameters)
+        .unwrap()
+        .clone();
+    // model tidyness -- random seed, automatic shutdown
+    context.init_random(parameters.seed);
+    context.add_plan(parameters.max_time, |context| {
+        context.shutdown();
+    });
+    // make it easy for the user to see what the parameters are
+    println!("{parameters:?}");
+    // if we've gotten to this point, nothing failed so return
+    // context wrapped in Ok so that the user knows nothing failed
     Ok(context)
 }
 
 fn main() {
-    let mut context = initialize().expect("Error initializing.");
+    let args = Args::parse();
+    let mut context = initialize(&args).expect("Error initializing.");
     context.execute();
 }
