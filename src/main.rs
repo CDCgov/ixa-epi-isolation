09219ce513d680eaf9017c18637c3eb1c9aee92b
Chirag Kumar <kzs9@cdc.gov>
2024-11-21 18:57:15 +0000

    code commenting

--- src/main.rs
+++ src/main.rs
@@ -26,16 +26,22 @@ mod parameters_loader;
 
 fn initialize(args: &Args) -> Result<Context, IxaError> {
     let mut context = Context::new();
+    // read the global properties, setting them as parameters
+    // propagate any errors that may arise with the ? operator
     parameters_loader::init_parameters(&mut context, &args.input_file)?;
     let parameters = context
         .get_global_property_value(Parameters)
         .unwrap()
         .clone();
+    // model tidyness -- random seed, automatic shutdown
     context.init_random(parameters.seed);
     context.add_plan(parameters.max_time, |context| {
         context.shutdown();
     });
+    // make it easy for the user to see what the parameters are
     println!("{parameters:?}");
+    // if we've gotten to this point, nothing failed so return
+    // context wrapped in Ok so that the user knows nothing failed
     Ok(context)
 }
 
