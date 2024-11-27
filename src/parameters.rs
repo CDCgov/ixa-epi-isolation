de31231cdaea4c40ea574ddbc36357c60872c15d
Chirag Kumar <kzs9@cdc.gov>
2024-11-27 11:41:28 -0500

    Validate inputs when reading them in (#8)
    * validation fcn
    
    * review comments
    
    * pre-commit fixes
    
    * new ixa syntax for validator in define_global_properties
    
    * playing withpre-commit
    
    * updated pathbuf syntax
    
    * explicit error messages in tests
    
    * add population_periodic_report to parameters values in test

--- src/parameters.rs
+++ src/parameters.rs
@@ -1,8 +1,7 @@
-use ixa::error::IxaError;
 use std::fmt::Debug;
 use std::path::PathBuf;
 
-use ixa::define_global_property;
+use ixa::{define_global_property, error::IxaError};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -16,4 +15,59 @@ pub struct ParametersValues {
     pub synth_population_file: PathBuf,
     pub population_periodic_report: String,
 }
-define_global_property!(Parameters, ParametersValues);
+
+fn validate_inputs(parameters: &ParametersValues) -> Result<(), IxaError> {
+    if parameters.r_0 < 0.0 {
+        return Err(IxaError::IxaError(
+            "r_0 must be a non-negative number.".to_string(),
+        ));
+    }
+    if parameters.generation_interval <= 0.0 {
+        return Err(IxaError::IxaError(
+            "The generation interval must be positive.".to_string(),
+        ));
+    }
+    Ok(())
+}
+
+define_global_property!(Parameters, ParametersValues, |p| { validate_inputs(p) });
+
+#[cfg(test)]
+mod test {
+    use super::validate_inputs;
+    use std::path::PathBuf;
+
+    use crate::parameters::ParametersValues;
+
+    #[test]
+    #[should_panic(expected = "r_0 must be a non-negative number.")]
+    fn test_validate_r_0() {
+        let parameters = ParametersValues {
+            max_time: 100.0,
+            seed: 0,
+            r_0: -1.0,
+            infection_duration: 5.0,
+            generation_interval: 5.0,
+            report_period: 1.0,
+            synth_population_file: PathBuf::from("."),
+            population_periodic_report: String::new(),
+        };
+        validate_inputs(&parameters).unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "The generation interval must be positive.")]
+    fn test_validate_gi() {
+        let parameters = ParametersValues {
+            max_time: 100.0,
+            seed: 0,
+            r_0: 2.5,
+            infection_duration: 5.0,
+            generation_interval: 0.0,
+            report_period: 1.0,
+            synth_population_file: PathBuf::from("."),
+            population_periodic_report: String::new(),
+        };
+        validate_inputs(&parameters).unwrap();
+    }
+}
