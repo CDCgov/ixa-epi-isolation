7af1b8cf9892da9b192c755a64a4873ddc25ac3f
Will Koval <ad71@cdc.gov>
2024-12-10 14:52:57 +0000

    Made transmission query symmetrical with nested hash

--- src/intervention_manager.rs
+++ src/intervention_manager.rs
@@ -1,5 +1,3 @@
-//relative infectiousness trait extension
-
 use ixa::{
     define_data_plugin,  
     define_person_property, 
@@ -9,6 +7,10 @@ use ixa::{
      PersonId, 
 };
 use std::collections::HashMap;
+use crate::transmission_manager::{
+    InfectiousStatusType, 
+    InfectiousStatus
+};
 
 #[derive(Debug, PartialEq, Clone, Copy, Eq, Hash)]
 pub enum FacemaskStatusType {
@@ -16,49 +18,46 @@ pub enum FacemaskStatusType {
     Wearing,
 }
 struct InterventionContainer {
-    intervention_map: HashMap<FacemaskStatusType, f64>,
+    intervention_map: HashMap<InfectiousStatusType, HashMap<FacemaskStatusType, f64>>,
 }
 
 define_data_plugin!(
     InterventionPlugin,
     InterventionContainer,
     InterventionContainer {
-        intervention_map: HashMap::<FacemaskStatusType, f64>::new(),
+        intervention_map: HashMap::new(),
     }
 );
 
 define_person_property_with_default!(FacemaskStatus, FacemaskStatusType, FacemaskStatusType::None);
 
 pub trait ContextInterventionExt {
-    fn query_relative_infectiousness(&self, transmitter_id: PersonId) -> f64;
-    fn query_relative_susceptiblity(&self, contact_id: PersonId) -> f64;
-    fn register_intervention(&mut self, facemask_status: FacemaskStatusType, relative_infectiousness: f64);
+    fn query_relative_transmission(&self, person_id: PersonId) -> f64;
+    fn register_intervention(&mut self, infectious_status: InfectiousStatusType, facemask_status: FacemaskStatusType, relative_transmission: f64);
 }
 
 impl ContextInterventionExt for Context {
-    fn query_relative_infectiousness(&self, transmitter_id: PersonId) -> f64 {
-        let facemask_status = self.get_person_property(transmitter_id, FacemaskStatus);
+    fn query_relative_transmission(&self, person_id: PersonId) -> f64 {
+        let facemask_status = self.get_person_property(person_id, FacemaskStatus);
+        let infectious_status = self.get_person_property(person_id, InfectiousStatus);
 
         *self.get_data_container(InterventionPlugin)
             .unwrap()
             .intervention_map
+            .get(&infectious_status)
+            .unwrap()
             .get(&facemask_status)
             .unwrap()
-
     }
 
-    fn query_relative_susceptiblity(&self, contact_id: PersonId) -> f64 {
-        let facemask_status = self.get_person_property(contact_id, FacemaskStatus);
-        match facemask_status {
-            FacemaskStatusType::None => 1.0,
-            FacemaskStatusType::Wearing => 0.5,
-        }
-    }
+    fn register_intervention(&mut self, infectious_status: InfectiousStatusType, facemask_status: FacemaskStatusType, relative_transmission: f64) {
+        let mut facemask_map = HashMap::new();
 
-    fn register_intervention(&mut self, facemask_status: FacemaskStatusType, relative_infectiousness: f64) {
+        facemask_map.insert(facemask_status, relative_transmission);
+
         self.get_data_container_mut(InterventionPlugin)
             .intervention_map
-            .insert(facemask_status, relative_infectiousness);
+            .insert(infectious_status, facemask_map);
     }
 }
 
@@ -68,14 +67,14 @@ mod test {
     use ixa::{people::ContextPeopleExt, Context};
 
     #[test]
-    fn test_query_relative_infectiousness() {
+    fn test_query_relative_transmission() {
         let mut context = Context::new();
-        let transmitter_id = context.add_person(()).unwrap();
-        context.register_intervention(FacemaskStatusType::Wearing, 0.5);
+        let contact_id = context.add_person(()).unwrap();
 
-        context.set_person_property(transmitter_id, FacemaskStatus, FacemaskStatusType::Wearing);
+        context.register_intervention(InfectiousStatusType::Susceptible, FacemaskStatusType::Wearing, 0.5);
+        context.set_person_property(contact_id, FacemaskStatus, FacemaskStatusType::Wearing);
 
-        let relative_infectiousness = context.query_relative_infectiousness(transmitter_id);
-        assert_eq!(relative_infectiousness, 0.5);
+        let relative_transmission = context.query_relative_transmission(contact_id);
+        assert_eq!(relative_transmission, 0.5);
     }
 }
