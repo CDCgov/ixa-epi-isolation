61c7bf11172dfb726f0d40fc9ebe7289a892918b
Eric Q. Mooring <pgv5@cdc.gov>
2024-12-12 09:11:36 -0900

    exploring comparison to ODE models

---
+++ scripts/create_simple_homog_pop.R
@@ -0,0 +1,9 @@
+# generate simple, unstructured synthetic population of arbitrary size
+n <- 1e3
+
+simple_people <- data.frame("age" = rep(1, times = n),
+                            "homeId" = "10000000000")
+
+write.csv(x = simple_people,
+          file = "./input/simple_people.csv",
+          row.names = FALSE)
