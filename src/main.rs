4e0b250918cb03f2ed63e295733b1c99cdd2b6bd
Guido Espana <ukd0@cdc.gov>
2024-12-23 18:54:50 +0000

    Update runner from ixa to not use custom arguments

--- src/main.rs
+++ src/main.rs
@@ -3,77 +3,60 @@ mod parameters;
 mod population_loader;
 mod transmission_manager;
 
-use clap::Args;
-use ixa::runner::run_with_custom_args;
+use ixa::runner::run_with_args;
 use ixa::{
-    Context, ContextGlobalPropertiesExt, ContextPeopleExt, ContextRandomExt, ContextReportExt,
-    IxaError,
+    ContextGlobalPropertiesExt, ContextPeopleExt, ContextRandomExt,
+    ContextReportExt,
 };
-use std::path::PathBuf;
 use transmission_manager::InfectiousStatus;
 
 use crate::parameters::Parameters;
 use crate::population_loader::{Age, CensusTract};
 
-#[derive(Args, Debug)]
-struct CustomArgs {
-    ///Whether force overwrite of output files if they already exist
-    #[arg(short = 'f', long)]
-    force_overwrite: bool,
-}
-
-fn initialize() -> Result<Context, IxaError> {
-    let mut context = run_with_custom_args(|context, args, custom_args: Option<CustomArgs>| {
+fn main() {
+    
+    run_with_args(|context, args, _| {
         // Read the global properties.
-        let custom_args = custom_args.unwrap();
         // Set the output directory and whether to overwrite the existing file.
         context
             .report_options()
-            .directory(PathBuf::from(&args.output_dir))
-            .overwrite(custom_args.force_overwrite);
+            .directory(args.output_dir.unwrap())
+            .overwrite(args.force_overwrite);
+
+        let parameters = context
+            .get_global_property_value(Parameters)
+            .unwrap()
+            .clone();
+
+        // Set the random seed.
+        context.init_random(parameters.seed);
+
+        // Report the number of people by age, census tract, and infectious status
+        // every report_period.
+        context.add_periodic_report(
+            "person_property_count",
+            parameters.report_period,
+            (Age, CensusTract, InfectiousStatus),
+        )?;
+
+        // Load the synthetic population from the `synthetic_population_file`
+        // specified in input.json.
+        population_loader::init(context)?;
+        context.index_property(Age);
+        context.index_property(CensusTract);
+
+        // Initialize the person-to-person transmission workflow.
+        transmission_manager::init(context);
+
+        // Add a plan to shut down the simulation after `max_time`, regardless of
+        // what else is happening in the model.
+        context.add_plan(parameters.max_time, |context| {
+            context.shutdown();
+        });
+
+        // Print out the parameters for debugging purposes for the user.
+        println!("{parameters:?}");
         Ok(())
-    })
-    .unwrap();
-
-    let parameters = context
-        .get_global_property_value(Parameters)
-        .unwrap()
-        .clone();
-
-    // Set the random seed.
-    context.init_random(parameters.seed);
-
-    // Report the number of people by age, census tract, and infectious status
-    // every report_period.
-    context.add_periodic_report(
-        "person_property_count",
-        parameters.report_period,
-        (Age, CensusTract, InfectiousStatus),
-    )?;
-
-    // Load the synthetic population from the `synthetic_population_file`
-    // specified in input.json.
-    population_loader::init(&mut context)?;
-    context.index_property(Age);
-    context.index_property(CensusTract);
-
-    // Initialize the person-to-person transmission workflow.
-    transmission_manager::init(&mut context);
-
-    // Add a plan to shut down the simulation after `max_time`, regardless of
-    // what else is happening in the model.
-    context.add_plan(parameters.max_time, |context| {
-        context.shutdown();
-    });
-
-    // Print out the parameters for debugging purposes for the user.
-    println!("{parameters:?}");
-
-    // Return context for execution.
-    Ok(context)
+    }).unwrap();
 }
 
-fn main() {
-    let mut context = initialize().expect("Error initializing.");
-    context.execute();
-}
