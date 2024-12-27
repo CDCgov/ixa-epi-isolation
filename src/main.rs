638eccdbbe1edd38c6563be502203948bb9d0004
Guido Espana <ukd0@cdc.gov>
2024-12-27 21:30:19 +0000

    remove unnecessary options

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,8 @@ use crate::parameters::Parameters;
 use crate::population_loader::{Age, CensusTract};
 
 fn main() {
-    run_with_args(|context, args, _| {
+    run_with_args(|context, _, _| {
         // Read the global properties.
-        // Set the output directory and whether to overwrite the existing file.
-        context
-            .report_options()
-            .directory(args.output_dir.unwrap())
-            .overwrite(args.force_overwrite);
-
         let parameters = context
             .get_global_property_value(Parameters)
             .unwrap()
