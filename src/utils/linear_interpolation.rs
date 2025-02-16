8c2ccde1b23e4221a840c0cdf41e8f6113e1b1b2
Chirag Kumar <kzs9@cdc.gov>
2025-02-16 16:50:17 +0000

    corner cases for EmpiricalRate struct, reorg, tests for math fcns

--- src/utils/linear_interpolation.rs
+++ src/utils/linear_interpolation.rs
@@ -1,10 +1,23 @@
 #[must_use]
-pub fn linear_interpolation(x1: f64, x2: f64, y1: f64, y2: f64, x: f64) -> f64 {
-    y1 + (y2 - y1) * (x - x1) / (x2 - x1)
+pub fn linear_interpolation(x1: f64, x2: f64, y1: f64, y2: f64, xp: f64) -> f64 {
+    y1 + (y2 - y1) / (x2 - x1) * (xp - x1)
 }
 
 #[cfg(test)]
 mod test {
+    use statrs::assert_almost_eq;
+
+    use super::linear_interpolation;
+
     #[test]
-    fn test_linear_interpolation() {}
+    fn test_linear_interpolation_simple() {
+        let result = linear_interpolation(1.0, 2.0, 3.0, 6.0, 1.25);
+        assert_almost_eq!(result, 3.75, 0.0);
+    }
+
+    #[test]
+    fn test_linear_exterpolation_simple() {
+        let result = linear_interpolation(1.0, 2.0, 3.0, 6.0, 2.5);
+        assert_almost_eq!(result, 7.5, 0.0);
+    }
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,4 +2,5 @@ pub mod linear_interpolation;
 pub use linear_interpolation::linear_interpolation;
 
 pub mod midpoint_integration;
+pub use midpoint_integration::cumulative_integral;
 pub use midpoint_integration::midpoint_integration;
