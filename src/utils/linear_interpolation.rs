41950a8cfc4bfe720f11ae4af523d4b2c109e9b6
Chirag Kumar <kzs9@cdc.gov>
2025-02-17 00:15:43 +0000

    handle initial push to cum_rates

--- src/utils/linear_interpolation.rs
+++ src/utils/linear_interpolation.rs
@@ -1,5 +1,9 @@
 #[must_use]
 pub fn linear_interpolation(x1: f64, x2: f64, y1: f64, y2: f64, xp: f64) -> f64 {
+    #[allow(clippy::float_cmp)]
+    if x1 == x2 {
+        return (y1 + y2) / 2.0;
+    }
     y1 + (y2 - y1) / (x2 - x1) * (xp - x1)
 }
 
@@ -19,5 +23,11 @@ mod test {
     fn test_linear_exterpolation_simple() {
         let result = linear_interpolation(1.0, 2.0, 3.0, 6.0, 2.5);
         assert_almost_eq!(result, 7.5, 0.0);
+    }
+
+    #[test]
+    fn test_linear_interpolation_same_x() {
+        let result = linear_interpolation(1.0, 1.0, 3.0, 6.0, 1.0);
+        assert_almost_eq!(result, 4.5, 0.0);
     }
 }
