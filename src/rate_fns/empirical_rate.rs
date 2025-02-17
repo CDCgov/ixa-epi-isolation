5b574a291745ff0efcace348d332b15c4be0317a
Chirag Kumar <kzs9@cdc.gov>
2025-02-17 01:55:06 +0000

    invertible function test + documentation

--- src/rate_fns/empirical_rate.rs
+++ src/rate_fns/empirical_rate.rs
@@ -24,7 +24,7 @@ impl EmpiricalRate {
             ));
         }
         if times[0] != 0.0 {
-            return Err(IxaError::IxaError("`times` must start 0.0".to_string()));
+            return Err(IxaError::IxaError("`times` must start at 0.0".to_string()));
         }
         if !times.is_sorted() {
             return Err(IxaError::IxaError(
@@ -40,12 +40,16 @@ impl EmpiricalRate {
         cum_rates
             .iter_mut()
             .for_each(|x| *x += instantaneous_rate[0]);
-        cum_rates.push(instantaneous_rate[0]);
+        cum_rates.insert(0, instantaneous_rate[0]);
         Ok(Self {
             times,
             instantaneous_rate,
             cum_rates,
         })
+    }
+    #[allow(dead_code)]
+    fn get_cum_rates(&self) -> Vec<f64> {
+        self.cum_rates.clone()
     }
 }
 
@@ -81,7 +85,8 @@ impl InfectiousnessRateFn for EmpiricalRate {
         cum_rate += midpoint_integration(
             &[self.times[lower_index], t],
             &[self.instantaneous_rate[lower_index], estimated_rate],
-        );
+        )
+        .unwrap();
         cum_rate
     }
 
@@ -112,19 +117,126 @@ fn get_lower_index(xs: &[f64], xp: f64) -> usize {
         // require samples of the rate function to start at time = 0.0.
         // We subtract 1 normally because binary search returns the index of the where `xp` would
         // fit, which is one after the closest value in `xs`.
-        Err(i) => usize::max(i - 1, 0),
+        Err(i) => usize::max(i, 1) - 1,
     }
 }
 
 #[cfg(test)]
-#[allow(clippy::float_cmp)]
 mod test {
-    #[test]
-    fn test_rate() {}
+    use ixa::IxaError;
+    use statrs::assert_almost_eq;
+
+    use super::{get_lower_index, EmpiricalRate};
+    use crate::{rate_fns::InfectiousnessRateFn, utils::linear_interpolation};
 
     #[test]
-    fn test_cum_rate() {}
+    fn test_get_lower_index_not_included_but_inclusive() {
+        let xs = vec![1.0, 2.0, 3.0];
+        assert_eq!(get_lower_index(&xs, 2.5), 1);
+    }
 
     #[test]
-    fn test_inverse_cum_rate() {}
+    fn test_get_lower_index_included() {
+        let xs = vec![1.0, 2.0, 3.0];
+        assert_eq!(get_lower_index(&xs, 3.0), 2);
+    }
+
+    #[test]
+    fn test_get_lower_index_not_included_not_inclusive() {
+        let xs = vec![1.0, 2.0, 3.0];
+        assert_eq!(get_lower_index(&xs, 0.5), 0);
+    }
+
+    #[test]
+    fn test_empirical_rate_times_instantaneous_rate_len_mismatch() {
+        let times = vec![0.0, 1.0, 2.0];
+        let instantaneous_rate = vec![0.0, 1.0];
+        let e = EmpiricalRate::new(times, instantaneous_rate).err();
+        match e {
+            Some(IxaError::IxaError(msg)) => {
+                assert_eq!(msg, "`times` and `instantaneous_rate` must have the same length".to_string());
+            }
+            Some(ue) => panic!(
+                "Expected an error that `times` and `instantaneous_rate` must be the same length. Instead got {:?}",
+                ue.to_string()
+            ),
+            None => panic!("Expected an error. Instead, passed with no errors."),
+        }
+    }
+
+    #[test]
+    fn test_empirical_rate_times_start_at_0() {
+        let times = vec![1.0, 2.0];
+        let instantaneous_rate = vec![0.0, 1.0];
+        let e = EmpiricalRate::new(times, instantaneous_rate).err();
+        match e {
+            Some(IxaError::IxaError(msg)) => {
+                assert_eq!(msg, "`times` must start at 0.0".to_string());
+            }
+            Some(ue) => panic!(
+                "Expected an error that `times` must start at 0.0. Instead got {:?}",
+                ue.to_string()
+            ),
+            None => panic!("Expected an error. Instead, passed with no errors."),
+        }
+    }
+
+    #[test]
+    fn test_empirical_rate_times_not_sorted() {
+        let times = vec![0.0, 0.0, 3.0, 2.0];
+        let instantaneous_rate = vec![0.0, 1.0, 2.0, 3.0];
+        let e = EmpiricalRate::new(times, instantaneous_rate).err();
+        match e {
+            Some(IxaError::IxaError(msg)) => {
+                assert_eq!(msg, "`times` must be sorted in ascending order".to_string());
+            }
+            Some(ue) => panic!(
+                "Expected an error that `times` must be sorted. Instead got {:?}",
+                ue.to_string()
+            ),
+            None => panic!("Expected an error. Instead, passed with no errors."),
+        }
+    }
+
+    #[test]
+    fn test_rate() {
+        let empirical = EmpiricalRate::new(vec![0.0, 1.0, 2.0], vec![0.0, 1.0, 2.0]).unwrap();
+        assert_almost_eq!(empirical.rate(1.5), 1.5, 0.0);
+    }
+
+    #[test]
+    fn test_cum_rate_vector() {
+        let empirical = EmpiricalRate::new(vec![0.0, 1.0, 2.0], vec![0.0, 1.0, 2.0]).unwrap();
+        assert_eq!(empirical.get_cum_rates(), vec![0.0, 0.5, 2.0]);
+    }
+
+    #[test]
+    fn test_cum_rate_eval() {
+        let empirical = EmpiricalRate::new(vec![0.0, 1.0, 2.0], vec![0.0, 1.0, 2.0]).unwrap();
+        assert_almost_eq!(empirical.cum_rate(1.5), 1.125, 0.0);
+    }
+
+    #[test]
+    fn test_inverse_cum_rate_in_bounds() {
+        let empirical = EmpiricalRate::new(vec![0.0, 1.0, 2.0], vec![0.0, 1.0, 2.0]).unwrap();
+        assert_eq!(
+            empirical.inverse_cum_rate(1.125),
+            // The rate function is linear, so it's integral (cum rate) is quadratic, and not
+            // linearly invertible. This is why we don't get exactly 1.5 here.
+            // We show exact linear invertibility in the next test.
+            Some(linear_interpolation(0.5, 2.0, 1.0, 2.0, 1.125))
+        );
+    }
+
+    #[test]
+    fn test_inverse_cum_rate_in_bounds_invertible() {
+        let empirical = EmpiricalRate::new(vec![0.0, 1.0, 2.0], vec![1.0, 1.0, 1.0]).unwrap();
+        assert_eq!(empirical.inverse_cum_rate(1.5), Some(0.5));
+    }
+
+    #[test]
+    fn test_inverse_cum_rate_out_bounds() {
+        let empirical = EmpiricalRate::new(vec![0.0, 1.0, 2.0], vec![0.0, 1.0, 2.0]).unwrap();
+        assert_eq!(empirical.inverse_cum_rate(2.1), None);
+    }
 }
