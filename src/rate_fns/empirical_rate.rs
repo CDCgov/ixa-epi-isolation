56ef31d2bd0d8e20d37a60a2fc8bbdd9f810beb9
Chirag Kumar <kzs9@cdc.gov>
2025-02-19 02:37:22 +0000

    add some more tests to show corner cases

--- src/rate_fns/empirical_rate.rs
+++ src/rate_fns/empirical_rate.rs
@@ -20,15 +20,25 @@ impl EmpiricalRate {
     pub fn new(times: Vec<f64>, instantaneous_rate: Vec<f64>) -> Result<Self, IxaError> {
         if times.len() != instantaneous_rate.len() {
             return Err(IxaError::IxaError(
-                "`times` and `instantaneous_rate` must have the same length".to_string(),
+                "`times` and `instantaneous_rate` must have the same length.".to_string(),
             ));
         }
         if times[0] != 0.0 {
-            return Err(IxaError::IxaError("`times` must start at 0.0".to_string()));
+            return Err(IxaError::IxaError("`times` must start at 0.0.".to_string()));
+        }
+        if times.len() <= 1 {
+            return Err(IxaError::IxaError(
+                "`times` and `instantaneous_rate` must have at least two elements.".to_string(),
+            ));
+        }
+        if instantaneous_rate.iter().any(|&x| x < 0.0) {
+            return Err(IxaError::IxaError(
+                "`instantaneous_rate` must be non-negative.".to_string(),
+            ));
         }
         if !times.is_sorted() {
             return Err(IxaError::IxaError(
-                "`times` must be sorted in ascending order".to_string(),
+                "`times` must be sorted in ascending order.".to_string(),
             ));
         }
         // We need to use the running cumulative integral to estimate the inverse rate, so calculate
@@ -71,7 +81,8 @@ impl EmpiricalRate {
 
 impl InfectiousnessRateFn for EmpiricalRate {
     fn rate(&self, t: f64) -> f64 {
-        self.lower_index_and_rate(t).2
+        // Ensure the rate cannot be negative
+        f64::max(0.0, self.lower_index_and_rate(t).2)
     }
     fn cum_rate(&self, t: f64) -> f64 {
         // Integrate rate function up until lower index -- over all times in the samples of the rate
@@ -104,9 +115,11 @@ impl InfectiousnessRateFn for EmpiricalRate {
     }
 }
 
-/// Get the index of the largest value in `xs` that is less than or equal to `xp`
-/// If there are multiple instances of that value in `xs`, a deterministically random one's index is
-/// returned.
+/// Get the index of the largest value in `xs` that is less than or equal to `xp`, and the index of
+/// the largest value in `xs` that is less than or equal to `xp` but accounts for there being at
+/// least one value in `xs` that is greater.
+/// If there are multiple instances of the greatest value less than or equal to `xp` in `xs`, a
+/// deterministically random one's index is returned.
 fn get_lower_index(xs: &[f64], xp: f64) -> (usize, usize) {
     let integration_index = match xs.binary_search_by(|x| x.partial_cmp(&xp).unwrap()) {
         Ok(i) => i,
@@ -137,19 +150,24 @@ mod test {
 
     #[test]
     fn test_get_lower_index_not_included_but_inclusive() {
-        let xs = vec![1.0, 2.0, 3.0];
+        let xs = vec![1.0, 2.0, 3.0, 4.0];
+        assert_eq!(get_lower_index(&xs, 3.5), (2, 2));
         assert_eq!(get_lower_index(&xs, 2.5), (1, 1));
+        assert_eq!(get_lower_index(&xs, 1.5), (0, 0));
     }
 
     #[test]
     fn test_get_lower_index_included() {
-        let xs = vec![1.0, 2.0, 3.0];
-        assert_eq!(get_lower_index(&xs, 3.0), (2, 1));
+        let xs = vec![1.0, 2.0, 3.0, 4.0];
+        assert_eq!(get_lower_index(&xs, 4.0), (3, 2));
+        assert_eq!(get_lower_index(&xs, 3.0), (2, 2));
+        assert_eq!(get_lower_index(&xs, 2.0), (1, 1));
+        assert_eq!(get_lower_index(&xs, 1.0), (0, 0));
     }
 
     #[test]
     fn test_get_lower_index_not_included_not_inclusive_below() {
-        let xs = vec![1.0, 2.0, 3.0];
+        let xs = vec![1.0, 2.0];
         assert_eq!(get_lower_index(&xs, 0.5), (0, 0));
     }
 
@@ -166,7 +184,7 @@ mod test {
         let e = EmpiricalRate::new(times, instantaneous_rate).err();
         match e {
             Some(IxaError::IxaError(msg)) => {
-                assert_eq!(msg, "`times` and `instantaneous_rate` must have the same length".to_string());
+                assert_eq!(msg, "`times` and `instantaneous_rate` must have the same length.".to_string());
             }
             Some(ue) => panic!(
                 "Expected an error that `times` and `instantaneous_rate` must be the same length. Instead got {:?}",
@@ -183,10 +201,44 @@ mod test {
         let e = EmpiricalRate::new(times, instantaneous_rate).err();
         match e {
             Some(IxaError::IxaError(msg)) => {
-                assert_eq!(msg, "`times` must start at 0.0".to_string());
+                assert_eq!(msg, "`times` must start at 0.0.".to_string());
             }
             Some(ue) => panic!(
                 "Expected an error that `times` must start at 0.0. Instead got {:?}",
+                ue.to_string()
+            ),
+            None => panic!("Expected an error. Instead, passed with no errors."),
+        }
+    }
+
+    #[test]
+    fn test_empirical_rate_times_at_least_len_two() {
+        let times = vec![0.0];
+        let instantaneous_rate = vec![0.0];
+        let e = EmpiricalRate::new(times, instantaneous_rate).err();
+        match e {
+            Some(IxaError::IxaError(msg)) => {
+                assert_eq!(msg, "`times` and `instantaneous_rate` must have at least two elements.".to_string());
+            }
+            Some(ue) => panic!(
+                "Expected an error that `times` and `instantaneous_rate` must have at least two elements. Instead got {:?}",
+                ue.to_string()
+            ),
+            None => panic!("Expected an error. Instead, passed with no errors."),
+        }
+    }
+
+    #[test]
+    fn test_empirical_rate_times_non_negative() {
+        let times = vec![0.0, 1.0];
+        let instantaneous_rate = vec![0.0, -0.2];
+        let e = EmpiricalRate::new(times, instantaneous_rate).err();
+        match e {
+            Some(IxaError::IxaError(msg)) => {
+                assert_eq!(msg, "`instantaneous_rate` must be non-negative.".to_string());
+            }
+            Some(ue) => panic!(
+                "Expected an error that `instantaneous_rate` must be non-negative. Instead got {:?}",
                 ue.to_string()
             ),
             None => panic!("Expected an error. Instead, passed with no errors."),
@@ -200,7 +252,10 @@ mod test {
         let e = EmpiricalRate::new(times, instantaneous_rate).err();
         match e {
             Some(IxaError::IxaError(msg)) => {
-                assert_eq!(msg, "`times` must be sorted in ascending order".to_string());
+                assert_eq!(
+                    msg,
+                    "`times` must be sorted in ascending order.".to_string()
+                );
             }
             Some(ue) => panic!(
                 "Expected an error that `times` must be sorted. Instead got {:?}",
@@ -238,6 +293,12 @@ mod test {
     fn test_rate() {
         let empirical = EmpiricalRate::new(vec![0.0, 1.0, 2.0], vec![0.0, 1.0, 2.0]).unwrap();
         assert_almost_eq!(empirical.rate(1.5), 1.5, 0.0);
+    }
+
+    #[test]
+    fn test_rate_not_negative() {
+        let empirical = EmpiricalRate::new(vec![0.0, 1.0, 2.0], vec![0.0, 1.0, 2.0]).unwrap();
+        assert_almost_eq!(empirical.rate(-0.5), 0.0, 0.0);
     }
 
     #[test]
