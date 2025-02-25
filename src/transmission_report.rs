3e271301f4bbbebe9af607166c825cab1b21e349
Will Koval <ad71@cdc.gov>
2025-02-25 18:40:48 +0000

    minor test change

--- src/transmission_report.rs
+++ src/transmission_report.rs
@@ -121,6 +121,8 @@ mod test {
         let source = context.add_person(()).unwrap();
         let target = context.add_person(()).unwrap();
         let infection_time = 1.0;
+        
+        context.infect_person(source, None);
 
         context.add_plan(infection_time, move |context| {
             context.infect_person(target, Some(source));
