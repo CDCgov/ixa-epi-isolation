d575ead755ff47d412f3ce2c3156648918b241eb
Chirag Kumar <kzs9@cdc.gov>
2025-03-19 03:33:49 +0000

    messy with lots of Box Any but works for storing tracers

--- src/clinical_status_manager.rs
+++ src/clinical_status_manager.rs
@@ -1,15 +1,32 @@
-use ixa::{Context, ContextPeopleExt, PersonProperty, PersonPropertyChangeEvent};
+use std::{
+    any::{Any, TypeId},
+    collections::HashMap,
+};
+
+use ixa::{
+    define_data_plugin, Context, ContextPeopleExt, PersonProperty, PersonPropertyChangeEvent,
+};
 
 pub trait ClinicalHealthStatus {
-    type Value;
-    fn next(&self, last: &Self::Value) -> Option<(Self::Value, f64)>;
+    fn next(&self, last: Box<dyn Any>) -> Option<(Box<dyn Any>, f64)>;
 }
+
+#[derive(Default)]
+struct ClinicalProgressionContainer {
+    progressions: HashMap<TypeId, Vec<Box<dyn ClinicalHealthStatus>>>,
+}
+
+define_data_plugin!(
+    ClinicalProgression,
+    ClinicalProgressionContainer,
+    ClinicalProgressionContainer::default()
+);
 
 pub trait ContextClinicalExt {
     fn register_clinical_progression<T: PersonProperty + 'static>(
         &mut self,
         property: T,
-        tracer: impl ClinicalHealthStatus<Value = T::Value> + 'static,
+        tracer: impl ClinicalHealthStatus + 'static,
     );
 }
 
@@ -17,16 +34,32 @@ impl ContextClinicalExt for Context {
     fn register_clinical_progression<T: PersonProperty + 'static>(
         &mut self,
         property: T,
-        tracer: impl ClinicalHealthStatus<Value = T::Value> + 'static,
+        tracer: impl ClinicalHealthStatus + 'static,
     ) {
-        self.subscribe_to_event(move |context, event: PersonPropertyChangeEvent<T>| {
-            if let Some((value, t)) = tracer.next(&event.current) {
-                let current_time = context.get_current_time();
-                context.add_plan(current_time + t, move |ctx| {
-                    ctx.set_person_property(event.person_id, property, value);
-                });
-            }
-        });
+        // Add tracer to data container
+        // Subscribe to event if hashmap has not yet considered this person property
+        // Make sure to get the right tracer out for the person
+        let container = self.get_data_container_mut(ClinicalProgression);
+        let progressions = container.progressions.entry(TypeId::of::<T>()).or_default();
+        progressions.push(Box::new(tracer));
+        if progressions.len() == 1 {
+            self.subscribe_to_event(move |context, event: PersonPropertyChangeEvent<T>| {
+                let container = context.get_data_container(ClinicalProgression).unwrap();
+                let progressions = container.progressions.get(&TypeId::of::<T>()).unwrap();
+                // Just for argument's sake, let's only ever take the first tracer.
+                let tcr = &progressions[0];
+                if let Some((next_value, time_to_next)) = tcr.next(Box::new(event.current)) {
+                    let current_time = context.get_current_time();
+                    context.add_plan(current_time + time_to_next, move |ctx| {
+                        ctx.set_person_property(
+                            event.person_id,
+                            property,
+                            *next_value.downcast_ref::<T::Value>().unwrap(),
+                        );
+                    });
+                }
+            });
+        }
     }
 }
 
