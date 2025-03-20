1c119be45af02981de93f217e66fcac83aa77f0b
Chirag Kumar <kzs9@cdc.gov>
2025-03-20 22:09:31 +0000

    tests, documentation, and tailoring use case more to ours

--- src/symptom_progression.rs
+++ src/symptom_progression.rs
@@ -1,4 +1,4 @@
-use ixa::{define_person_property_with_default, Context};
+use ixa::{define_person_property_with_default, warn, Context, IxaError};
 use serde::Serialize;
 
 use crate::clinical_status_manager::{ClinicalHealthStatus, ContextClinicalExt};
@@ -10,7 +10,6 @@ pub enum DiseaseSeverityValue {
     Asymptomatic,
     Mild,
     Moderate,
-    Severe,
 }
 
 define_person_property_with_default!(
@@ -19,17 +18,43 @@ define_person_property_with_default!(
     DiseaseSeverityValue::Healthy
 );
 
+/// Holds a sequence of unique states and times between subsequent states for changing a person's
+/// person property values accordingly. Since all person properties that have Markovian transitions
+/// may want to have their progressions defined empirically, we make this a general struct that can
+/// hold any type `T` that implements `PartialEq` and `Copy`.
 pub struct EmpiricalProgression<T: PartialEq + Copy> {
     states: Vec<T>,
     time_to_next: Vec<f64>,
 }
 
 impl<T: PartialEq + Copy> EmpiricalProgression<T> {
-    pub fn new(states: Vec<T>, time_to_next: Vec<f64>) -> EmpiricalProgression<T> {
-        EmpiricalProgression {
+    /// Makes a new `EmpiricalProgression<T>` struct that holds a sequence of states of value `T`.
+    /// Assumes values in `states` are unique.
+    /// # Errors
+    /// - If `states` is not one element longer than `time_to_next`.
+    pub fn new(
+        states: Vec<T>,
+        time_to_next: Vec<f64>,
+    ) -> Result<EmpiricalProgression<T>, IxaError> {
+        if states.len() != time_to_next.len() + 1 {
+            return Err(IxaError::IxaError(
+                "Size mismatch: states must be one element longer than time_to_next. Instead, "
+                    .to_string()
+                    + &format!(
+                        "states has length {} and time_to_next has length {}.",
+                        states.len(),
+                        time_to_next.len()
+                    ),
+            ));
+        }
+        warn!(
+            "Adding an EmpiricalProgression. At this time, we do not check whether values in
+        states are unique."
+        );
+        Ok(EmpiricalProgression {
             states,
             time_to_next,
-        }
+        })
     }
 }
 
@@ -48,38 +73,39 @@ impl<T: PartialEq + Copy> ClinicalHealthStatus for EmpiricalProgression<T> {
     }
 }
 
-pub fn init(context: &mut Context) {
+pub fn init(context: &mut Context) -> Result<(), IxaError> {
     // Todo(kzs9): We will read these progressions from a file from our isolation guidance modeling
-    let progression1 = EmpiricalProgression::new(
+    // For now, these are example possible progressions based on our isolation guidance modeling
+    let example_progression_cat1 = EmpiricalProgression::new(
+        vec![
+            DiseaseSeverityValue::Presymptomatic,
+            DiseaseSeverityValue::Moderate,
+            DiseaseSeverityValue::Healthy,
+        ],
+        vec![1.0, 4.0],
+    )?;
+    context.register_clinical_progression(DiseaseSeverity, example_progression_cat1);
+
+    let example_progression_cat2 = EmpiricalProgression::new(
         vec![
             DiseaseSeverityValue::Presymptomatic,
             DiseaseSeverityValue::Mild,
-            DiseaseSeverityValue::Moderate,
-            DiseaseSeverityValue::Severe,
             DiseaseSeverityValue::Healthy,
         ],
-        vec![1.0, 2.0, 1.0, 4.0],
-    );
-    context.register_clinical_progression(DiseaseSeverity, progression1);
+        vec![2.0],
+    )?;
+    context.register_clinical_progression(DiseaseSeverity, example_progression_cat2);
 
-    let progression2 = EmpiricalProgression::new(
+    let example_progression_asymptomatic = EmpiricalProgression::new(
         vec![
             DiseaseSeverityValue::Asymptomatic,
             DiseaseSeverityValue::Healthy,
         ],
-        vec![2.0],
-    );
-    context.register_clinical_progression(DiseaseSeverity, progression2);
-
-    let progression3 = EmpiricalProgression::new(
-        vec![
-            DiseaseSeverityValue::Presymptomatic,
-            DiseaseSeverityValue::Mild,
-            DiseaseSeverityValue::Healthy,
-        ],
         vec![2.0, 3.0],
-    );
-    context.register_clinical_progression(DiseaseSeverity, progression3);
+    )?;
+    context.register_clinical_progression(DiseaseSeverity, example_progression_asymptomatic);
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -87,7 +113,7 @@ mod test {
     use super::{DiseaseSeverityValue, EmpiricalProgression};
     use crate::clinical_status_manager::ClinicalHealthStatus;
 
-    use ixa::Context;
+    use ixa::{Context, IxaError};
     use statrs::assert_almost_eq;
 
     #[test]
@@ -100,7 +126,8 @@ mod test {
                 DiseaseSeverityValue::Mild,
             ],
             vec![1.0, 2.0],
-        );
+        )
+        .unwrap();
         let initial_state = DiseaseSeverityValue::Presymptomatic;
         let (next_state, time) = progression.next(&context, &initial_state).unwrap();
         assert_eq!(next_state, DiseaseSeverityValue::Asymptomatic);
@@ -114,5 +141,28 @@ mod test {
         let initial_state = DiseaseSeverityValue::Mild;
         let next_state = progression.next(&context, &initial_state);
         assert!(next_state.is_none());
+    }
+
+    #[test]
+    fn test_empirical_progression_errors_len() {
+        let progression = EmpiricalProgression::new(
+            vec![
+                DiseaseSeverityValue::Presymptomatic,
+                DiseaseSeverityValue::Asymptomatic,
+                DiseaseSeverityValue::Mild,
+            ],
+            vec![1.0, 2.0, 3.0],
+        );
+        let e = progression.err();
+        match e {
+            Some(IxaError::IxaError(msg)) => {
+                assert_eq!(msg, "Size mismatch: states must be one element longer than time_to_next. Instead, states has length 3 and time_to_next has length 3.".to_string());
+            }
+            Some(ue) => panic!(
+                "Expected an error that states and time_to_next have incompatible sizes. Instead got {:?}",
+                ue.to_string()
+            ),
+            None => panic!("Expected an error. Instead, validation passed with no errors."),
+        }
     }
 }
