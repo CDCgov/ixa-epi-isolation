e45211b3aaee90f9f5f90ab8590c4cf0d71b21ef
Guido Espana <ukd0@cdc.gov>
2025-03-25 16:26:00 +0000

    included schools and workplaces in synthetic population script

--- src/population_loader.rs
+++ src/population_loader.rs
@@ -7,32 +7,56 @@ use serde::Deserialize;
 use std::path::PathBuf;
 
 use crate::parameters::ContextParametersExt;
+use crate::settings::{
+    ContextSettingExt,
+    ItineraryEntry,SettingId,
+    Home, Workplace, School, CensusTract
+};
 
 #[derive(Deserialize, Debug)]
 #[allow(non_snake_case)]
 pub struct PeopleRecord<'a> {
     age: u8,
     homeId: &'a [u8],
+    schoolId: &'a [u8],
+    workplaceId: &'a [u8],
 }
 
 define_person_property!(Age, u8);
 define_person_property_with_default!(Alive, bool, true);
 
 define_person_property!(Household, usize);
-define_person_property!(CensusTract, usize);
+define_person_property!(CensusTractId, usize);
+define_person_property!(SchoolId, Option<usize>);
+define_person_property!(WorkplaceId, Option<usize>);
+
 
 fn create_person_from_record(
     context: &mut Context,
     person_record: &PeopleRecord,
 ) -> Result<(), IxaError> {
+    let mut itinerary_person = Vec::<ItineraryEntry>::new();
     let tract: String = String::from_utf8(person_record.homeId[..11].to_owned())?;
     let home_id: String = String::from_utf8(person_record.homeId.to_owned())?;
+    let school_string: String = String::from_utf8(person_record.schoolId.to_owned())?;
+    let workplace_string: String = String::from_utf8(person_record.workplaceId.to_owned())?;
 
-    let _person_id = context.add_person((
-        (Age, person_record.age),
-        (Household, home_id.parse()?),
-        (CensusTract, tract.parse()?),
-    ))?;
+    // First, build itinerary
+    itinerary_person.push(ItineraryEntry::new(&SettingId::<Home>::new(home_id.parse()?), 0.25));
+    if !school_string.is_empty() {
+        itinerary_person.push(ItineraryEntry::new(&SettingId::<School>::new(school_string.parse()?), 0.25));
+    }
+    if !workplace_string.is_empty() {
+        itinerary_person.push(ItineraryEntry::new(&SettingId::<Workplace>::new(workplace_string.parse()?), 0.25));
+    }
+    itinerary_person.push(ItineraryEntry::new(&SettingId::<CensusTract>::new(tract.parse()?), 0.25));
+    
+   
+    let person_id = context.add_person(
+        (Age, person_record.age)
+    )?;
+    
+    context.add_itinerary(person_id, itinerary_person)?;
 
     Ok(())
 }
@@ -51,6 +75,12 @@ fn load_synth_population(context: &mut Context, synth_input_file: PathBuf) -> Re
 
 pub fn init(context: &mut Context) -> Result<(), IxaError> {
     let parameters = context.get_params();
+
+    context.register_setting_type(Home {}, SettingProperties { alpha: 0.1 });
+    context.register_setting_type(CensusTract {}, SettingProperties { alpha: 0.01 });
+    context.register_setting_type(Workplace {}, SettingProperties { alpha: 0.05 });
+    context.register_setting_type(School {}, SettingProperties { alpha: 0.01 });
+
     load_synth_population(context, parameters.synth_population_file.clone())
 }
 
@@ -72,7 +102,9 @@ mod test {
     #[test]
     fn check_synth_file_tract() {
         let mut context = Context::new();
-        let input = String::from("age,homeId\n43,360930331020001\n42,360930331020002");
+        let input = String::from(
+            "age,homeId,schoolId,workplaceId\n43,360930331020001,,\n42,360930331020002,,",
+        );
         let synth_file = persist_tmp_csv(&input);
         load_synth_population(&mut context, synth_file).unwrap();
         let age = [43, 42];
@@ -86,7 +118,7 @@ mod test {
                 1,
                 context.query_people_count((
                     (Age, age[i]),
-                    (CensusTract, tract[i]),
+                    (CensusTractId, tract[i]),
                     (Household, home_id[i]),
                 ))
             );
@@ -97,8 +129,63 @@ mod test {
     #[should_panic(expected = "range end index 11 out of range for slice of length 9")]
     fn check_invalid_census_tract() {
         let mut context = Context::new();
-        let input = String::from("age,homeId\n43,360930331\n42,360930331020002");
+        let input =
+            String::from("age,homeId,schoolId,workplaceId\n43,360930331,,\n42,360930331020002,,");
         let synth_file = persist_tmp_csv(&input);
         load_synth_population(&mut context, synth_file).unwrap();
+    }
+
+    #[test]
+    fn check_synth_file_school() {
+        let mut context = Context::new();
+        let input = String::from(
+            "age,homeId,schoolId,workplaceId\n43,360930331020001,1,\n42,360930331020002,2,",
+        );
+        let synth_file = persist_tmp_csv(&input);
+        load_synth_population(&mut context, synth_file).unwrap();
+        let age = [43, 42];
+        let school_id = [1, 2];
+        let home_id = [360_930_331_020_001, 360_930_331_020_002];
+
+        assert_eq!(context.get_current_population(), 2);
+
+        for i in 0..1 {
+            assert_eq!(
+                1,
+                context.query_people_count((
+                    (Age, age[i]),
+                    (SchoolId, Some(school_id[i])),
+                    (Household, home_id[i]),
+                    (WorkplaceId, None)
+                ))
+            );
+        }
+    }
+
+    #[test]
+    fn check_synth_file_workplace() {
+        let mut context = Context::new();
+        let input = String::from(
+            "age,homeId,schoolId,workplaceId\n43,360930331020001,,1\n42,360930331020002,,2",
+        );
+        let synth_file = persist_tmp_csv(&input);
+        load_synth_population(&mut context, synth_file).unwrap();
+        let age = [43, 42];
+        let workplace_id = [1, 2];
+        let home_id = [360_930_331_020_001, 360_930_331_020_002];
+
+        assert_eq!(context.get_current_population(), 2);
+
+        for i in 0..1 {
+            assert_eq!(
+                1,
+                context.query_people_count((
+                    (Age, age[i]),
+                    (Household, home_id[i]),
+                    (SchoolId, None),
+                    (WorkplaceId, Some(workplace_id[i]))
+                ))
+            );
+        }
     }
 }
