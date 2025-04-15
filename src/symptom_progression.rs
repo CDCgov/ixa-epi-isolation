6ae8bb4695602e8de2d777af93a6a7dae3289a59
Chirag Kumar <chirag9kumar@gmail.com>
2025-04-15 10:17:01 -0400

    trying to simplify while staying away from examples

--- src/symptom_progression.rs
+++ src/symptom_progression.rs
@@ -11,12 +11,11 @@ use crate::{
 
 #[derive(PartialEq, Copy, Clone, Debug, Serialize)]
 pub enum IsolationGuidanceSymptomValue {
-    NoSymptoms,
+    Presymptomatic,
     Category1,
     Category2,
     Category3,
     Category4,
-    Improved,
 }
 
 define_person_property_with_default!(
@@ -26,59 +25,37 @@ define_person_property_with_default!(
 );
 
 pub fn init(context: &mut Context) -> Result<(), IxaError> {
-    // Todo(kzs9): We will read a library of symptom progressions (no symptoms for an incubation
-    // period, some symptom category for Stan-calculated symptom improvement time, then improved)
-    // from our isolation guidance Stan modeling. This is a follow-up PR and filed as an issue.
-    // For now, I provide a few examples of symptom progressions to show how they will be structured
-    let example_progression_cat1 = EmpiricalProgression::new(
-        vec![
-            Some(IsolationGuidanceSymptomValue::NoSymptoms),
-            Some(IsolationGuidanceSymptomValue::Category1),
-            Some(IsolationGuidanceSymptomValue::Improved),
-        ],
-        vec![1.0, 8.0],
-    )?;
-    context.register_property_progression(IsolationGuidanceSymptom, example_progression_cat1);
+    // Todo(kzs9): We will read a library of symptom progressions from a file based on our Stan
+    // modeling. That file will contain a set of possible symptom progressions -- a sequence of
+    // symptom categories and times. This is a follow-up PR and filed as an issue.
+    // For now, we demonstrate how these progressions would be constructed and registered.
+    for cat in 0..5 {
+        let cat_name = match cat {
+            0 => IsolationGuidanceSymptomValue::Presymptomatic,
+            1 => IsolationGuidanceSymptomValue::Category1,
+            2 => IsolationGuidanceSymptomValue::Category2,
+            3 => IsolationGuidanceSymptomValue::Category3,
+            4 => IsolationGuidanceSymptomValue::Category4,
+            _ => unreachable!(),
+        };
+        let incubation_period = 4.0;
+        let symptom_duration = 5.0;
+        let progression = EmpiricalProgression::new(
+            vec![
+                Some(IsolationGuidanceSymptomValue::Presymptomatic),
+                Some(cat_name),
+                None,
+            ],
+            vec![incubation_period, symptom_duration],
+        )?;
+        context.register_property_progression(IsolationGuidanceSymptom, progression);
+    }
 
-    let example_progression_cat2 = EmpiricalProgression::new(
-        vec![
-            Some(IsolationGuidanceSymptomValue::NoSymptoms),
-            Some(IsolationGuidanceSymptomValue::Category2),
-            Some(IsolationGuidanceSymptomValue::Improved),
-        ],
-        vec![2.0, 4.0],
-    )?;
-    context.register_property_progression(IsolationGuidanceSymptom, example_progression_cat2);
-
-    let example_progression_cat3 = EmpiricalProgression::new(
-        vec![
-            Some(IsolationGuidanceSymptomValue::NoSymptoms),
-            Some(IsolationGuidanceSymptomValue::Category3),
-            Some(IsolationGuidanceSymptomValue::Improved),
-        ],
-        vec![3.0, 3.0],
-    )?;
-    context.register_property_progression(IsolationGuidanceSymptom, example_progression_cat3);
-
-    let example_progression_cat4 = EmpiricalProgression::new(
-        vec![
-            Some(IsolationGuidanceSymptomValue::NoSymptoms),
-            Some(IsolationGuidanceSymptomValue::Category4),
-            Some(IsolationGuidanceSymptomValue::Improved),
-        ],
-        vec![5.0, 1.0],
-    )?;
-    context.register_property_progression(IsolationGuidanceSymptom, example_progression_cat4);
-
-    let example_progression_asymptomatic = EmpiricalProgression::new(
-        vec![
-            Some(IsolationGuidanceSymptomValue::NoSymptoms),
-            Some(IsolationGuidanceSymptomValue::Improved),
-        ],
+    let asymptomatic_progression = EmpiricalProgression::new(
+        vec![Some(IsolationGuidanceSymptomValue::Presymptomatic), None],
         vec![4.0],
     )?;
-    context
-        .register_property_progression(IsolationGuidanceSymptom, example_progression_asymptomatic);
+    context.register_property_progression(IsolationGuidanceSymptom, asymptomatic_progression);
 
     event_subscriptions(context);
 
@@ -92,7 +69,7 @@ fn event_subscriptions(context: &mut Context) {
                 context.set_person_property(
                     event.person_id,
                     IsolationGuidanceSymptom,
-                    Some(IsolationGuidanceSymptomValue::NoSymptoms),
+                    Some(IsolationGuidanceSymptomValue::Presymptomatic),
                 );
             }
         },
@@ -144,12 +121,12 @@ mod test {
         context.infect_person(person, None);
         context.execute();
         // The only progression that we know for certainty is that the person will not be
-        // `NoSymptoms` at the end of the simulation.
+        // `Presymptomatic` at the end of the simulation.
         assert!(context
             .get_person_property(person, IsolationGuidanceSymptom)
             .is_some());
         assert!(
-            Some(IsolationGuidanceSymptomValue::NoSymptoms)
+            Some(IsolationGuidanceSymptomValue::Presymptomatic)
                 != context.get_person_property(person, IsolationGuidanceSymptom)
         );
     }
