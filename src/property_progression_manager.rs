0f660e113f7b7edc38a855a4456ac1c48c91a1ca
Chirag Kumar <kzs9@cdc.gov>
2025-04-18 14:31:27 +0000

    tests that show overall use in property progression

--- src/property_progression_manager.rs
+++ src/property_progression_manager.rs
@@ -79,7 +79,11 @@ mod test {
 
     use std::any::TypeId;
 
-    use ixa::{Context, ContextPeopleExt, ContextRandomExt, ExecutionPhase};
+    use ixa::{
+        define_person_property_with_default, Context, ContextPeopleExt, ContextRandomExt,
+        ExecutionPhase, PersonPropertyChangeEvent,
+    };
+    use statrs::assert_almost_eq;
 
     use crate::population_loader::Age;
 
@@ -94,6 +98,23 @@ mod test {
         fn next(&self, _context: &Context, last: &Self::Value) -> Option<(Self::Value, f64)> {
             Some((*last + 1, self.time_to_next_age))
         }
+    }
+
+    #[test]
+    fn test_progression_trait() {
+        let progression = AgeProgression {
+            time_to_next_age: 1.0,
+        };
+        let context = Context::new();
+        let (next_value, time_to_next) = progression.next(&context, &0).unwrap();
+        assert_eq!(next_value, 1);
+        assert_almost_eq!(time_to_next, 1.0, 0.0);
+
+        let boxed = Box::new(progression) as Box<dyn Progression<Value = u8>>;
+        let tcr = boxed.as_ref();
+        let (next_value, time_to_next) = tcr.next(&context, &0).unwrap();
+        assert_eq!(next_value, 1);
+        assert_almost_eq!(time_to_next, 1.0, 0.0);
     }
 
     #[test]
@@ -165,5 +186,142 @@ mod test {
             .unwrap()
             .as_ref();
         assert_eq!(tcr.next(&context, &0_u8).unwrap(), (1_u8, 2.0));
+    }
+
+    define_person_property_with_default!(NumberRunningShoes, u8, 0);
+
+    struct RunningShoesProgression {
+        max: u8,
+        increase: u8,
+        time_to_next: f64,
+    }
+
+    impl Progression for RunningShoesProgression {
+        type Value = u8;
+        fn next(&self, _context: &Context, last: &Self::Value) -> Option<(Self::Value, f64)> {
+            if *last >= self.max {
+                return None;
+            }
+            Some((*last + self.increase, self.time_to_next))
+        }
+    }
+
+    #[test]
+    fn test_multiple_properties_registered() {
+        let mut context = Context::new();
+        context.init_random(0);
+        let age_progression = AgeProgression {
+            time_to_next_age: 1.0,
+        };
+        let shoe_progression = RunningShoesProgression {
+            max: 4,
+            increase: 2,
+            time_to_next: 0.5,
+        };
+        context.register_property_progression(Age, age_progression);
+        context.register_property_progression(NumberRunningShoes, shoe_progression);
+        // Get out the registered progressions.
+        let container = context.get_data_container(PropertyProgressions).unwrap();
+        let age_progressions = container.progressions.get(&TypeId::of::<Age>()).unwrap();
+        assert_eq!(age_progressions.len(), 1);
+        let shoes_progressions = container
+            .progressions
+            .get(&TypeId::of::<NumberRunningShoes>())
+            .unwrap();
+        assert_eq!(shoes_progressions.len(), 1);
+        // See that the progressions do what they are supposed to do.
+        let person = context.add_person((Age, 0)).unwrap();
+        context.set_person_property(person, Age, 0);
+        context.set_person_property(person, NumberRunningShoes, 0);
+        // Number of running shoes increase by 2 every 0.5 time units until greater than 4.
+        context.add_plan_with_phase(
+            0.5,
+            move |ctx| {
+                let n = ctx.get_person_property(person, NumberRunningShoes);
+                assert_eq!(n, 2);
+            },
+            ExecutionPhase::Last,
+        );
+        context.add_plan_with_phase(
+            1.0,
+            move |ctx| {
+                let n = ctx.get_person_property(person, NumberRunningShoes);
+                assert_eq!(n, 4);
+            },
+            ExecutionPhase::Last,
+        );
+        // There should never ever be any more edits to the number of running shoes beyond 4.
+        context.subscribe_to_event(
+            move |_, event: PersonPropertyChangeEvent<NumberRunningShoes>| {
+                assert!(event.current <= 4);
+            },
+        );
+        context.add_plan_with_phase(
+            1.0,
+            move |ctx| {
+                let age = ctx.get_person_property(person, Age);
+                assert_eq!(age, 1);
+            },
+            ExecutionPhase::Last,
+        );
+        context.add_plan_with_phase(
+            2.0,
+            move |ctx| {
+                let age = ctx.get_person_property(person, Age);
+                assert_eq!(age, 2);
+            },
+            ExecutionPhase::Last,
+        );
+        context.add_plan_with_phase(3.0, Context::shutdown, ExecutionPhase::Last);
+        context.execute();
+    }
+
+    struct ShoesMultiplyProgression {
+        multiplier: u8,
+        time_to_next: f64,
+    }
+
+    impl Progression for ShoesMultiplyProgression {
+        type Value = u8;
+        fn next(&self, _context: &Context, last: &Self::Value) -> Option<(Self::Value, f64)> {
+            Some((*last * self.multiplier, self.time_to_next))
+        }
+    }
+
+    #[test]
+    fn test_multiple_implementations() {
+        let mut context = Context::new();
+        let running_shoes_progression = RunningShoesProgression {
+            max: 4,
+            increase: 2,
+            time_to_next: 0.5,
+        };
+        let shoes_multiply_progression = ShoesMultiplyProgression {
+            multiplier: 2,
+            time_to_next: 0.5,
+        };
+        context.register_property_progression(NumberRunningShoes, running_shoes_progression);
+        context.register_property_progression(NumberRunningShoes, shoes_multiply_progression);
+        // Get out the registered progressions.
+        let container = context.get_data_container(PropertyProgressions).unwrap();
+        let shoes_progressions = container
+            .progressions
+            .get(&TypeId::of::<NumberRunningShoes>())
+            .unwrap();
+        assert_eq!(shoes_progressions.len(), 2);
+        // See that the progressions do what they are supposed to do even though they are different
+        // structs -- in other words, what matters is that they implement the same trait (`Progression`).
+        // Inspect the first progression
+        let tcr = shoes_progressions[0]
+            .downcast_ref::<Box<dyn Progression<Value = u8>>>()
+            .unwrap()
+            .as_ref();
+        assert_eq!(tcr.next(&context, &0_u8).unwrap(), (2_u8, 0.5));
+        // Same for the second
+        let tcr = shoes_progressions[1]
+            .downcast_ref::<Box<dyn Progression<Value = u8>>>()
+            .unwrap()
+            .as_ref();
+        assert_eq!(tcr.next(&context, &1_u8).unwrap(), (2_u8, 0.5));
     }
 }
