fc71a26bae21e5d6a8c523ea68d888691cb89492
Chirag Kumar <chirag9kumar@gmail.com>
2025-04-20 19:39:25 -0400

    tests for natural history parameter manager

--- src/natural_history_parameters_manager.rs
+++ src/natural_history_parameters_manager.rs
@@ -2,7 +2,6 @@ use std::{
     any::TypeId,
     cell::RefCell,
     collections::{hash_map::Entry, HashMap},
-    rc::Rc,
 };
 
 use ixa::{define_data_plugin, define_rng, Context, ContextRandomExt, IxaError, PersonId};
@@ -10,7 +9,7 @@ use ixa::{define_data_plugin, define_rng, Context, ContextRandomExt, IxaError, P
 define_rng!(NaturalHistoryParametersRng);
 
 pub trait NaturalHistoryParameter {
-    fn library_size(&self) -> usize;
+    fn library_size(&self, context: &Context) -> usize;
 }
 
 pub trait NaturalHistoryId {
@@ -27,8 +26,8 @@ type IdAssigner<I> = dyn Fn(&Context, PersonId) -> I;
 
 #[derive(Default)]
 struct NaturalHistoryParametersContainer {
-    parameter_assignments: HashMap<TypeId, Box<IdAssigner<Box<dyn NaturalHistoryId>>>>,
-    ids: Rc<RefCell<HashMap<TypeId, HashMap<PersonId, usize>>>>,
+    parameter_id_assigners: HashMap<TypeId, Box<IdAssigner<Box<dyn NaturalHistoryId>>>>,
+    ids: RefCell<HashMap<TypeId, HashMap<PersonId, usize>>>,
 }
 
 define_data_plugin!(
@@ -82,21 +81,20 @@ impl ContextNaturalHistoryParametersExt for Context {
         
         // Only register this assignment function if this parameter does not have a previously
         // registered assignment function.
-        match container.parameter_assignments.entry(TypeId::of::<T>()) {
-            Entry::Vacant(_) => {
-                container.parameter_assignments.insert(
-                    TypeId::of::<T>(),
+        match container.parameter_id_assigners.entry(TypeId::of::<T>()) {
+            Entry::Vacant(entry) => {
+                entry.insert(
                     Box::new(move |ctx, person_id| Box::new(assignment_fn(ctx, person_id))),
                 );
+                Ok(())
             }
 
             Entry::Occupied(_) => {
-                return Err(IxaError::IxaError(
+                Err(IxaError::IxaError(
                     "An assignment function for this parameter has already been registered.".to_string(),
-                ));
+                ))
             }
         }
-        Ok(())
     }
 
     fn get_parameter_id<T>(&self, parameter: T, person_id: PersonId) -> usize
@@ -115,7 +113,7 @@ impl ContextNaturalHistoryParametersExt for Context {
         }
 
         // Is there an assignment function for this parameter?
-        if let Some(assigner) = container.parameter_assignments.get(&TypeId::of::<T>()) {
+        if let Some(assigner) = container.parameter_id_assigners.get(&TypeId::of::<T>()) {
             // Get the id from the assignment
             let id = assigner(self, person_id).id();
             // Store the id for this person
@@ -129,7 +127,7 @@ impl ContextNaturalHistoryParametersExt for Context {
         }
 
         // Else make a default random assignment and store it
-        let library_size = parameter.library_size();
+        let library_size = parameter.library_size(self);
         let id = self.sample_range(NaturalHistoryParametersRng, 0..library_size);
         container
             .ids
@@ -143,33 +141,93 @@ impl ContextNaturalHistoryParametersExt for Context {
 
 #[cfg(test)]
 mod test {
-    use ixa::{context::Context, ContextPeopleExt};
+    use std::any::TypeId;
 
-    use crate::population_loader::Age;
+    use ixa::{context::Context, ContextPeopleExt, ContextRandomExt, IxaError};
 
-    use super::{ContextNaturalHistoryParametersExt, NaturalHistoryParameter};
+    use super::{ContextNaturalHistoryParametersExt, NaturalHistoryParameter, NaturalHistoryParameters};
 
     struct ViralLoad;
 
     impl NaturalHistoryParameter for ViralLoad {
-        fn library_size(&self) -> usize {
+        fn library_size(&self, _context: &Context) -> usize {
             1
         }
     }
 
+    fn init_context() -> Context {
+        let mut context = Context::new();
+        context.init_random(0);
+        context
+    }
+
     #[test]
     fn test_register_vanilla_parameter_id_assignment() {
-
+        let mut context = init_context();
+        let person = context.add_person(()).unwrap();
+        context
+            .register_parameter_id_assignment(ViralLoad, |_, _| 0)
+            .unwrap();
+        let container = context.get_data_container(NaturalHistoryParameters).unwrap();
+        assert_eq!(container.parameter_id_assigners.len(), 1);
+        let assigner = container.parameter_id_assigners.get(&TypeId::of::<ViralLoad>()).unwrap();
+        assert_eq!(assigner(&context, person).id(), 0);
     }
 
     #[test]
     fn test_error_register_two_assignments_same_parameter() {
+        let mut context = init_context();
+        context
+            .register_parameter_id_assignment(ViralLoad, |_, _| 0)
+            .unwrap();
+        let result = context.register_parameter_id_assignment(ViralLoad, |_, _| 1);
+        let e = result.err();
+        match e {
+            Some(IxaError::IxaError(msg)) => {
+                assert_eq!(msg, "An assignment function for this parameter has already been registered.".to_string());
+            }
+            Some(ue) => panic!(
+                "Expected an error that an assignment function for this parameter has already been registered.. Instead got {:?}",
+                ue.to_string()
+            ),
+            None => panic!("Expected an error. Instead, function registration passed with no errors."),
+        }
+    }
 
+    struct AntigenPositivity;
+
+    impl NaturalHistoryParameter for AntigenPositivity {
+        fn library_size(&self, _context: &Context) -> usize {
+            1
+        }
+        
     }
 
     #[test]
     fn test_error_register_assignment_after_querying() {
-
+        let mut context = init_context();
+        let person = context.add_person(()).unwrap();
+        // We have to register something to make sure the data container exists.
+        context
+            .register_parameter_id_assignment(ViralLoad, |_, _| 0)
+            .unwrap();
+        // This section also tests 
+        let result = context.get_parameter_id(AntigenPositivity, person);
+        assert_eq!(result, 0);
+        let result = context.register_parameter_id_assignment(AntigenPositivity, |_, _| 1);
+        let e = result.err();
+        match e {
+            Some(IxaError::IxaError(msg)) => {
+                assert_eq!(msg, "An id for this parameter has been previously queried, so a new assignment function cannot be specified.
+                If this is desired behavior, register an assignment function that changes from random to the specified
+                behavior at the time at which this assignment is registered.".to_string());
+            }
+            Some(ue) => panic!(
+                "Expected an error that an id for this parameter has been previously queried so an assignment function cannot be registered. Instead got {:?}",
+                ue.to_string()
+            ),
+            None => panic!("Expected an error. Instead, function registration passed with no errors."),
+        }
     }
 
     #[test]
@@ -179,21 +237,56 @@ mod test {
 
     #[test]
     fn test_get_parameter_id_registered_assignment() {
-
+        let mut context = init_context();
+        let person = context.add_person(()).unwrap();
+        context
+            .register_parameter_id_assignment(ViralLoad, |_, _| 0)
+            .unwrap();
+        let id = context.get_parameter_id(ViralLoad, person);
+        assert_eq!(id, 0);
     }
 
     #[test]
     fn test_get_parameter_id_already_set() {
-
+        let mut context = init_context();
+        let person = context.add_person(()).unwrap();
+        let container = context
+            .get_data_container_mut(NaturalHistoryParameters);
+        container.ids.borrow_mut().insert(
+            TypeId::of::<ViralLoad>(),
+            vec![(person, 0)].into_iter().collect(),
+        );
+        let id = context.get_parameter_id(ViralLoad, person);
+        assert_eq!(id, 0);
     }
 
-    #[test]
-    fn test_get_parameter_id_random_assignment() {
+    struct CulturePositivity;
 
+    impl NaturalHistoryParameter for CulturePositivity {
+        fn library_size(&self, _context: &Context) -> usize {
+            10000
+        }
+    }
+
+    struct TestingPatterns;
+
+    impl NaturalHistoryParameter for TestingPatterns {
+        fn library_size(&self, _context: &Context) -> usize {
+            10000
+        }
     }
 
     #[test]
     fn test_get_parameter_id_assignment_has_dependencies() {
-
+        let mut context = init_context();
+        let person = context.add_person(()).unwrap();
+        context
+            .register_parameter_id_assignment(CulturePositivity, |context, person_id| {
+                context.get_parameter_id(TestingPatterns, person_id)
+            })
+            .unwrap();
+        let culture_id = context.get_parameter_id(CulturePositivity, person);
+        let testing_id = context.get_parameter_id(TestingPatterns, person);
+        assert_eq!(culture_id, testing_id);
     }
 }
