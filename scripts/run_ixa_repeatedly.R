79bef6ac39e09b62b44c0f4d1f6cd4fafc2d565d
Eric Q. Mooring <pgv5@cdc.gov>
2025-05-05 11:26:15 -0800

    updates, including not hardcoding file names

--- scripts/run_ixa_repeatedly.R
+++ scripts/run_ixa_repeatedly.R
@@ -3,22 +3,28 @@
 # so run the script directly from a terminal
 
 # function that changes the seed, runs the model, and gets the output
+# hard-coded that seed is in row 4 of the input json
 run_ixa_rep_fx <- function(ixa_rep) {
-  input_params$epi_isolation.Parameters$seed <- ixa_rep
-  jsonlite::toJSON(x = input_params, pretty = TRUE, auto_unbox = TRUE) |>
-    writeLines("./input/input.json")
-  system("cargo run -- -i ./input/input.json -o ./output -f")
+  print(paste("currently running replicate", ixa_rep, "of", ixa_reps, sep = " "))
+  input_json[4] <- gsub("\\d+", ixa_rep, input_json[4])
+  writeLines(input_json, args[1])
+  system(paste("cargo run -- -c", args[1], "-o ./output -f"))
   infectious_report <- readr::read_csv(file.path(
     "output",
     "person_property_count.csv"
-  )) |>
+  ), show_col_types = FALSE) |>
     dplyr::mutate(model = "ixa", ixa_rep = ixa_rep) |>
-    dplyr::select(!c(Age, CensusTract))
+    dplyr::select(!c(Age))
   return(infectious_report)
 }
 
-# read in the input JSON, of which the seed is one of the params
-input_params <- jsonlite::fromJSON("./input/input.json")
+# get the name of the JSON to use and the name of the output
+
+args <- commandArgs(trailingOnly = TRUE)
+
+# read in the input JSON, of which the seed is a component
+
+input_json <- readLines(args[1])
 
 # set number of reps
 ixa_reps <- 50
@@ -32,6 +38,6 @@ output_df <- do.call(rbind, output)
 # export results
 write.csv(
   x = output_df,
-  file = "./output/person_property_count_multi_rep.csv",
+  file = args[2],
   row.names = FALSE
 )
