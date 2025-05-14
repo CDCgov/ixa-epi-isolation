c2e3a9987e9512452a2a680082fa66e255d317ba
Erik Rosenstrom <xui6@cdc.gov>
2025-05-14 12:13:22 -0700

    adding unit test for infection location proportions

--- src/infection_propagation_loop.rs
+++ src/infection_propagation_loop.rs
@@ -97,6 +97,7 @@ mod test {
         Context, ContextGlobalPropertiesExt, ContextPeopleExt, ContextRandomExt, ExecutionPhase,
         IxaError, PersonId, PersonPropertyChangeEvent,
     };
+
     use statrs::{
         assert_almost_eq,
         distribution::{ContinuousCDF, Uniform},
@@ -115,7 +116,10 @@ mod test {
             ContextParametersExt, GlobalParams, ItinerarySpecificationType, Params, RateFnType,
         },
         rate_fns::{load_rate_fns, InfectiousnessRateExt},
-        settings::{ContextSettingExt, ItineraryEntry, SettingId, SettingProperties},
+        settings::{
+            init as settings_init, CensusTract, ContextSettingExt, Home, ItineraryEntry, SettingId,
+            SettingProperties, Workplace,
+        },
     };
 
     use super::{schedule_recovery, seed_infections};
@@ -158,6 +162,41 @@ mod test {
         context
             .register_setting_type(
                 HomogeneousMixing,
+                SettingProperties {
+                    alpha,
+                    itinerary_specification: Some(ItinerarySpecificationType::Constant {
+                        ratio: 1.0,
+                    }),
+                },
+            )
+            .unwrap();
+        context
+            .register_setting_type(
+                Home,
+                SettingProperties {
+                    alpha,
+                    itinerary_specification: Some(ItinerarySpecificationType::Constant {
+                        ratio: 1.0,
+                    }),
+                },
+            )
+            .unwrap();
+
+        context
+            .register_setting_type(
+                CensusTract,
+                SettingProperties {
+                    alpha,
+                    itinerary_specification: Some(ItinerarySpecificationType::Constant {
+                        ratio: 1.0,
+                    }),
+                },
+            )
+            .unwrap();
+
+        context
+            .register_setting_type(
+                Workplace,
                 SettingProperties {
                     alpha,
                     itinerary_specification: Some(ItinerarySpecificationType::Constant {
@@ -398,5 +437,138 @@ mod test {
         );
         // Make sure nothing has happened after person is recovered.
         assert_almost_eq!(context.get_current_time(), recovery_time, 0.0);
+    }
+
+    #[test]
+    fn test_location_infections_one_time_unit() {
+        // Does one infectious person generate the number of infections as expected by the rate?
+        // We're going to run many simulations that each start with one infectious and one
+        // susceptible person. The susceptible person gets moved back to susceptible when becoming
+        // infected, so this is really a setup where there is no susceptible depletion/an
+        // infinitely large starting population. We stop the simulation at the end of 1.0 time units
+        // and compare the number of infected people to the infectious rate.
+        // We're also going to check the times at which they are infected. In this test simulation,
+        // we are using a constant hazard of infection, and we only record infection times that are
+        // within 1.0 time units, so we expect the timing of infection attempts to follow U(0, 1).
+        // First, we should not expect to observe an exponential distribution because we may observe
+        // multiple infection attempts in the same experiment, not just the first. This also helps
+        // provide intuition for why we expect a uniform distribution -- if the first infection
+        // attempt happens quickly, that increases the chance we see another in 1.0 time units, and
+        // because there is basically this compensating relationship between the time and the number
+        // of events, they "cancel" each other out to give a uniform distribution (handwavingly).
+        let num_sims: u64 = 15_000;
+        let rate = 1.5;
+        let alpha = 0.42;
+        // We need the total infectiousness multiplier for the person.
+        let mut total_infectiousness_multiplier = None;
+        // Where we store the infection times.
+        let num_infected_home = Rc::new(RefCell::new(0usize));
+        let num_infected_censustract = Rc::new(RefCell::new(0usize));
+        let num_infected_workplace = Rc::new(RefCell::new(0usize));
+        for seed in 0..num_sims {
+            let num_infected_home_clone = Rc::clone(&num_infected_home);
+            let num_infected_cenustract_clone = Rc::clone(&num_infected_censustract);
+            let num_infected_workplace_clone = Rc::clone(&num_infected_workplace);
+            let mut context = setup_context(seed, rate, alpha);
+            settings_init(&mut context);
+            // We only run the simulation for 1.0 time units.
+            context.add_plan_with_phase(1.0, ixa::Context::shutdown, ExecutionPhase::Last);
+            // Add a a person who will get infected.
+            let infectious_person = context.add_person(()).unwrap();
+            let person_home = context.add_person(()).unwrap();
+            let person_censustract = context.add_person(()).unwrap();
+            let person_workplace = context.add_person(()).unwrap();
+            let itinerary_all = vec![
+                ItineraryEntry::new(&SettingId::new(Home, 0), 1.0),
+                ItineraryEntry::new(&SettingId::new(CensusTract, 0), 1.0),
+                ItineraryEntry::new(&SettingId::new(Workplace, 0), 1.0),
+            ];
+            let itinerary_home = vec![ItineraryEntry::new(&SettingId::new(Home, 0), 1.0)];
+            let itinerary_censustract =
+                vec![ItineraryEntry::new(&SettingId::new(CensusTract, 0), 1.0)];
+            let itinerary_workplace = vec![ItineraryEntry::new(&SettingId::new(Workplace, 0), 1.0)];
+            context
+                .add_itinerary(infectious_person, itinerary_all)
+                .unwrap();
+            context.add_itinerary(person_home, itinerary_home).unwrap();
+            context
+                .add_itinerary(person_censustract, itinerary_censustract)
+                .unwrap();
+            context
+                .add_itinerary(person_workplace, itinerary_workplace)
+                .unwrap();
+
+            // We don't want infectious people beyond our index case to be able to transmit, so we
+            // have to do setup on our own since just calling `init` will trigger a watcher for
+            // people becoming infectious that lets them transmit.
+            load_rate_fns(&mut context).unwrap();
+            // Add our infectious fellow.
+            let infectious_person = context.add_person(()).unwrap();
+
+            context.infect_person(infectious_person, None, None, None);
+            // Get the total infectiousness multiplier for comparison to total number of infections.
+            if total_infectiousness_multiplier.is_none() {
+                total_infectiousness_multiplier = Some(max_total_infectiousness_multiplier(
+                    &context,
+                    infectious_person,
+                ));
+            }
+            // Add a watcher for when people are infected to record the infection times.
+            context.subscribe_to_event::<PersonPropertyChangeEvent<InfectionStatus>>(
+                move |context, event| {
+                    if event.current == InfectionStatusValue::Infectious {
+                        // Reset the person to susceptible.
+                        if event.person_id == person_home {
+                            *num_infected_home_clone.borrow_mut() += 1;
+                            context.set_person_property(
+                                event.person_id,
+                                InfectionData,
+                                InfectionDataValue::Susceptible,
+                            );
+                        } else if event.person_id == person_censustract {
+                            *num_infected_cenustract_clone.borrow_mut() += 1;
+                            context.set_person_property(
+                                event.person_id,
+                                InfectionData,
+                                InfectionDataValue::Susceptible,
+                            );
+                        } else if event.person_id == person_workplace {
+                            *num_infected_workplace_clone.borrow_mut() += 1;
+                            context.set_person_property(
+                                event.person_id,
+                                InfectionData,
+                                InfectionDataValue::Susceptible,
+                            );
+                        }
+                    }
+                },
+            );
+            // Setup is now over -- onto actually letting our infectious fellow infect others.
+            schedule_next_forecasted_infection(&mut context, infectious_person);
+            context.execute();
+        }
+        #[allow(clippy::cast_precision_loss)]
+        let avg_number_infections_home = num_infected_home.take() as f64 / num_sims as f64;
+        assert_almost_eq!(
+            avg_number_infections_home,
+            rate * total_infectiousness_multiplier.unwrap() / 3.0,
+            0.05
+        );
+        #[allow(clippy::cast_precision_loss)]
+        let avg_number_infections_censustract =
+            num_infected_censustract.take() as f64 / num_sims as f64;
+        assert_almost_eq!(
+            avg_number_infections_censustract,
+            rate * total_infectiousness_multiplier.unwrap() / 3.0,
+            0.05
+        );
+        #[allow(clippy::cast_precision_loss)]
+        let avg_number_infections_workplace =
+            num_infected_workplace.take() as f64 / num_sims as f64;
+        assert_almost_eq!(
+            avg_number_infections_workplace,
+            rate * total_infectiousness_multiplier.unwrap() / 3.0,
+            0.05
+        );
     }
 }
