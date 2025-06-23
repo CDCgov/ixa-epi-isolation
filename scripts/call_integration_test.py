049f918f1b2232140d44c52cb9bebbe4366b8a69
KOVALW <AD71@cdc.gov>
2025-06-23 13:50:09 +0000

    different summary fn

--- scripts/call_integration_test.py
+++ scripts/call_integration_test.py
@@ -10,8 +10,13 @@ from abmwrappers.experiment_class import Experiment
 def main(config_file):
     if not os.path.exists("input/rate_fns_SIR.csv"):
         # These should create ALL of the rates, populations, and ODE output, not just the rate_fns_SIR.csv
-        subprocess.run("RScript scripts/create_empirical_rates.R".split())
-        subprocess.run("Rscript scripts/create_synthetic_pops.R".split())
+        subprocess.run("RScript scripts/create_integration_test_rate_fns.R".split())
+        subprocess.run("Rscript scripts/create_integration_test_pops.R".split())
+
+        if False:
+            subprocess.run("Rscript scripts/create_integration_test_ode_output.R".split())
+        else:
+            raise NotImplementedError("")
 
     experiment = Experiment(
         experiments_directory="tests",
@@ -22,7 +27,7 @@ def main(config_file):
     subprocess.run(f"Rscript scripts/create_ode_outputs.R {input}".split())
     simulation_df = wrappers.create_simulation_data(
         experiment=experiment, 
-        data_processing_fn=return_count_only
+        data_processing_fn=return_infection_count
     )
 
     # simulation_df.filter((pl.col("t") > 5) & (pl.col("t") < 10)).write_csv(
@@ -30,8 +35,18 @@ def main(config_file):
     # )
     print(simulation_df)
 
+def return_infection_count(directory: str):
+    file_path = os.path.join(directory, "person_property_count.csv")
+    if os.path.exists(file_path):
+        df = pl.read_csv(file_path)
+    else:
+        raise FileNotFoundError(f"Expected file not found: {file_path}")
 
-def return_count_only(directory: str):
+    df = df.group_by(["t", "InfectionStatus"]).agg(pl.col("count").sum())
+
+    return df
+
+def return_raw_count(directory: str):
     file_path = os.path.join(directory, "person_property_count.csv")
     if os.path.exists(file_path):
         df = pl.read_csv(file_path)
