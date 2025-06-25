6d5981cfd4107592cc0df58919dc022661946d2f
Eric Q. Mooring <pgv5@cdc.gov>
2025-06-25 09:59:01 -0800

    simplifications and ensure inputs generated first

--- scripts/call_integration_test.py
+++ scripts/call_integration_test.py
@@ -8,28 +8,16 @@ from abmwrappers.experiment_class import Experiment
 
 
 def main(config_file):
-    rate_fns_path = "tests/data/rate_fns_exp_I.csv"
+    rate_fns_path = "tests/input/rate_fns_exp_I.csv"
     if not os.path.exists(rate_fns_path):
         # These should create ALL of the rates, populations, and ODE output, not just the rate_fns_SIR.csv
-        subprocess.Popen("Rscript scripts/create_integration_test_rate_fns.R".split(), stdout=subprocess.PIPE)
-        subprocess.Popen("Rscript scripts/create_integration_test_pops.R".split(), stdout=subprocess.PIPE)
-
-        if False:
-            raise NotImplementedError("")
-            input = experiment.default_params_file
-            # In many cases - params from ODE have to hava fxnl relationship with the base JSON file
-            subprocess.run(f"Rscript scripts/create_ode_outputs.R {input}".split())
+        subprocess.run("Rscript scripts/create_integration_test_rate_fns.R".split())
+        subprocess.run("Rscript scripts/create_integration_test_pops.R".split())
+        subprocess.run("Rscript scripts/create_ode_output.R".split())
 
     experiment = Experiment(
         experiments_directory="tests",
-        config_file=config_file,
-        changed_baseline_params={
-            "infectiousness_rate_fn": {
-                "EmpiricalFromFile": {
-                    "file": rate_fns_path
-                }
-            }
-        }
+        config_file=config_file
     )
     simulation_df = wrappers.create_simulation_data(
         experiment=experiment, 
