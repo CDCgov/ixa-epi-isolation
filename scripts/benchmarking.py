f5b9c12b96729c1a7e4298f648fec3a06cc2ece8
Erik Rosenstrom <xui6@cdc.gov>
2025-07-09 14:08:26 -0700

    pre-commit removing files

--- scripts/benchmarking.py
+++ scripts/benchmarking.py
@@ -1,10 +1,13 @@
-from abmwrappers import  wrappers
-from abmwrappers.experiment_class import Experiment
+import json
 import os
 import time
-import polars as pl 
+
+import polars as pl
 import yaml
-import json
+from abmwrappers import wrappers
+from abmwrappers.experiment_class import Experiment
+
+
 def main():
     # Create the new Experiment and scenarios folder
     experiment = Experiment(
@@ -12,9 +15,7 @@ def main():
         config_file="scripts/experiments/benchmarking/jul25-assessment/input/config.yaml",
     )
 
-    wrappers.create_scenario_subexperiments(
-        experiment
-    )
+    wrappers.create_scenario_subexperiments(experiment)
 
     # Iterate over config files in the new scenarios directory
     # Create simulation data and store as parquet file in each scenario folder
@@ -26,27 +27,36 @@ def main():
         )
         with open(config_path, "r") as f:
             experiment_dict = yaml.safe_load(f)
-        
+
         subexperiment = Experiment(
             experiments_directory=experiment.directory,
             config_file=config_path,
         )
         start_time = time.time()
         wrappers.run_step_return_data(
-            experiment=subexperiment,
-            data_preprocessing_fn=read_fn
+            experiment=subexperiment, data_preprocessing_fn=read_fn
         )
         end_time = time.time()
-        average_time = (end_time - start_time) / experiment_dict["experiment_conditions"]["replicates_per_particle"]
-        
-        sim_input = os.path.join(
-            scenarios_dir, scenario, "input", "base.json"
-        )
+        average_time = (end_time - start_time) / experiment_dict[
+            "experiment_conditions"
+        ]["replicates_per_particle"]
+
+        sim_input = os.path.join(scenarios_dir, scenario, "input", "base.json")
         with open(sim_input, "r") as f:
             sim_data = json.load(f)
-        pop_size = pl.read_csv(sim_data[experiment_dict["experiment_conditions"]["scenario_key"]]["synth_population_file"]).height
-        average_times.append({"scenario": int(scenario.split("=")[-1]), "average_time": average_time, "pop_size": pop_size})
-        
+        pop_size = pl.read_csv(
+            sim_data[experiment_dict["experiment_conditions"]["scenario_key"]][
+                "synth_population_file"
+            ]
+        ).height
+        average_times.append(
+            {
+                "scenario": int(scenario.split("=")[-1]),
+                "average_time": average_time,
+                "pop_size": pop_size,
+            }
+        )
+
         experiment.simulation_bundles.update(
             {scenario: subexperiment.simulation_bundles[0]}
         )
@@ -59,9 +69,14 @@ def main():
     experiment.save(
         os.path.join(experiment.directory, "data", "experiment_history.pkl")
     )
-    exp_output = experiment.parquet_from_path(os.path.join(experiment.directory, "data", "scenarios"))
-    exp_output = exp_output.join(pl.DataFrame(average_times), on="scenario", how="left")
+    exp_output = experiment.parquet_from_path(
+        os.path.join(experiment.directory, "data", "scenarios")
+    )
+    exp_output = exp_output.join(
+        pl.DataFrame(average_times), on="scenario", how="left"
+    )
     print(exp_output)
+
 
 def read_fn(outputs_dir):
     output_file_path = os.path.join(outputs_dir, "person_property_count.csv")
@@ -71,13 +86,16 @@ def read_fn(outputs_dir):
         raise FileNotFoundError(f"{output_file_path} does not exist.")
     max_t = df["t"].max()
     df = (
-        df.group_by(["t", "InfectionStatus"])
-        .agg(pl.col("count").sum())
-        .filter(
-            (pl.col("InfectionStatus") == pl.lit("Recovered"))
+        (
+            df.group_by(["t", "InfectionStatus"])
+            .agg(pl.col("count").sum())
+            .filter((pl.col("InfectionStatus") == pl.lit("Recovered")))
         )
-    ).filter(pl.col("t") == max_t).select(pl.col("count").alias("attack_rate"))
+        .filter(pl.col("t") == max_t)
+        .select(pl.col("count").alias("attack_rate"))
+    )
     print(df.to_series().to_list()[0])
     return df
+
 
 main()
