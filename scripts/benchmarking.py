37f94079b4c501e5325bdce7dacc4980ce94080f
Erik Rosenstrom <xui6@cdc.gov>
2025-08-22 12:18:51 -0700

    adding parameters from griddle

--- scripts/benchmarking.py
+++ scripts/benchmarking.py
@@ -1,8 +1,10 @@
 import os
 import time
 
+import griddler
 import polars as pl
 from abmwrappers import wrappers
+from abmwrappers import utils
 from abmwrappers.experiment_class import Experiment
 import json
 
@@ -19,20 +21,33 @@ def main():
     # Iterate over config files in the new scenarios directory
     # Create simulation data and store as parquet file in each scenario folder
     scenarios_dir = os.path.join(experiment.directory, "scenarios")
-    average_times = []
+    parameters = []
     for scenario in os.listdir(scenarios_dir):
         config_path = os.path.join(
             scenarios_dir, scenario, "input", "config.yaml"
         )
-
         subexperiment = Experiment(
             experiments_directory=experiment.directory,
             config_file=config_path,
         )
-        start_time = time.time()
         subexperiment.run_step(data_read_fn=read_fn, products=["simulations"])
-        end_time = time.time()
-        average_time = (end_time - start_time) / subexperiment.replicates
+        with open(experiment.griddle_file, "r") as fp:
+            raw_griddle = json.load(fp)
+        print(type(raw_griddle))
+        print(raw_griddle["parameters"].keys())
+        parameter_dict = {}
+        for key in raw_griddle["parameters"].keys():
+            print(subexperiment.simulation_bundles[0].baseline_params[
+            subexperiment.scenario_key])
+            flattened_dict = utils.flatten_dict(subexperiment.simulation_bundles[0].baseline_params[
+            subexperiment.scenario_key])
+            parameter_dict[key] = flattened_dict[key]
+        parameter_dict["scenario"] = int(scenario.split("=")[-1])
+        parameters.append(parameter_dict)
+        # print(griddler.parse(raw_griddle))
+        
+        # for par_set in griddler.parse(raw_griddle)():
+        #     print(par_set)
 
         # synth_pop = subexperiment.simulation_bundles[0].baseline_params[
         #     subexperiment.scenario_key
@@ -85,30 +100,15 @@ def main():
             clean=False,
         )
     experiment.save()
+    parameter_df = pl.DataFrame(parameters)
     exp_output = experiment.read_results(filename="scenarios")
+    exp_output = exp_output.join(parameter_df, on="scenario")
     exp_output.write_csv(
         os.path.join(experiment.directory, "experiment_runtime.csv")
     )
 
 
 def read_fn(outputs_dir):
-    # output_file_path = os.path.join(outputs_dir, "person_property_count.csv")
-    # if os.path.exists(output_file_path):
-    #     df = pl.read_csv(output_file_path)
-    # else:
-    #     raise FileNotFoundError(f"{output_file_path} does not exist.")
-    # max_t = df["t"].max()
-    # df = (
-    #     (
-    #         df.group_by(["t", "infection_status"])
-    #         .agg(pl.col("count").sum())
-    #         .filter((pl.col("infection_status") == pl.lit("Recovered")))
-    #     )
-    #     .filter(pl.col("t") == max_t)
-    #     .select(pl.col("count").alias("attack_rate"))
-    # )
-    # print(df.to_series().to_list()[0])
-
     profiling_file_path = os.path.join(outputs_dir, "profiling_data.json")
     with open(profiling_file_path, "r") as f:
         profiling_data = json.load(f)
