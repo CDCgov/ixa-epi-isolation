7eefa60d3c5ee4482068068bd9d9458fbd09fbd0
KOVALW <AD71@cdc.gov>
2025-09-04 15:23:20 +0000

    throw error on period = 0.0

--- src/reports/mod.rs
+++ src/reports/mod.rs
@@ -28,17 +28,17 @@ pub fn init(context: &mut Context) -> Result<(), IxaError> {
     for report in &reports {
         match report {
             ReportType::PrevalenceReport { name, period } => {
-                if period < &0.0 {
+                if period <= &0.0 {
                     return Err(IxaError::IxaError(
-                        "The prevalence report writing period must be non-negative.".to_string(),
+                        "The prevalence report writing period must be greater than zero.".to_string(),
                     ));
                 }
                 prevalence_report::init(context, name.as_str(), *period)?;
             }
             ReportType::IncidenceReport { name, period } => {
-                if period < &0.0 {
+                if period <= &0.0 {
                     return Err(IxaError::IxaError(
-                        "The incidence report writing period must be non-negative.".to_string(),
+                        "The incidence report writing period must be greater than zero.".to_string(),
                     ));
                 }
                 incidence_report::init(context, name.as_str(), *period)?;
